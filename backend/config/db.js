e56cf7c25e366052736ad13f72bd24d33cfc13e5
Pramit Amatya <130182347+pumpking786@users.noreply.github.com>
2025-03-27 17:40:19 +0545

    Add files via upload

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+const sequelize = require("sequelize")
+const db=new sequelize('test','root','password',{
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+const connection=async()=>{
+    try {
+        await db.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error.message);
+      }
+}
+connection();
+
+module.exports=db;
