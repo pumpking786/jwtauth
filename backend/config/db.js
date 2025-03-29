eef8f4c98beafeff283615bf079898d2e0e7b5dc
Pramit Amatya <pramitamatya786@gmail.com>
2025-03-30 02:27:06 +0545

    ok

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,31 +1,31 @@
-const sequelize = require("sequelize");
-const config = require("../config/config.json")["development"];
-const db = new sequelize(config.database, config.username, config.password, {
-  host: config.host,
-  dialect: "mysql",
-});
+// const sequelize = require("sequelize");
+// const config = require("../config/config.json")["development"];
+// const db = new sequelize(config.database, config.username, config.password, {
+//   host: config.host,
+//   dialect: "mysql",
+// });
 
-// async function CreateTable() {
+// // async function CreateTable() {
+// //   try {
+// //     // Create tables(Other acts can be done through here)
+// //     await db.sync();
+// //     console.log("Db Table created!");
+// //   } catch (err) {
+// //     console.log("Error creating table", err.message);
+// //   }
+// // }
+
+// // // Call the function for the table
+// // CreateTable();
+
+// const connection = async () => {
 //   try {
-//     // Create tables(Other acts can be done through here)
-//     await db.sync();
-//     console.log("Db Table created!");
-//   } catch (err) {
-//     console.log("Error creating table", err.message);
+//     await db.authenticate();
+//     console.log("Connection has been established successfully.");
+//   } catch (error) {
+//     console.error("Unable to connect to the database:", error.message);
 //   }
-// }
+// };
+// connection();
 
-// // Call the function for the table
-// CreateTable();
-
-const connection = async () => {
-  try {
-    await db.authenticate();
-    console.log("Connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error.message);
-  }
-};
-connection();
-
-module.exports = db;
+// module.exports = db;
